ff5d30b7e227dac11a79ef119552f392cd4e5847
balaji410 <balaji410@gmail.com>
2025-06-03 15:28:03 +0530

    Create HelloWorldController.java

---
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping("/Hello")
+public String sayHello() {
+	System.out.println("Hello world from GET API");
+	
+	return "Hello world from GET API";
+}
+
+}
