0cd29dfacbbad789c1a010697b6f24cbb5af1a90
balaji410 <balaji410@gmail.com>
2025-06-03 15:10:44 +0530

    Update SprintbootHelloworldApplication.java

--- com/example/demo/SprintbootHelloworldApplication.java
+++ com/example/demo/SprintbootHelloworldApplication.java
@@ -1,1 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintbootHelloworldApplication.class, args);
+	}
+
+}
 
